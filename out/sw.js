8da92b56703e7a3b5229c3a86f325c6585aba743
Tanzeel Naveed Khan 💻 <tanzeelnaveed8@gmail.com>
2025-11-19 15:53:08 +0500

    Add files via upload

---
+++ out/sw.js
@@ -0,0 +1,17 @@
+self.addEventListener("install", (event) => {
+  console.log("Service Worker Installed");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker Activated");
+});
+
+// app offline open ho sake
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      return res || fetch(event.request);
+    })
+  );
+});
